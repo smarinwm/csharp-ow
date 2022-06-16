382219c3fa8e05cdf9a2c1fc28912af392aabb7a
SilverioUserSvrDev <SilverioUserSvrDev@WINDEV2204EVAL>
2022-06-16 12:37:51 +0200

    Create Datos.Repository\LibrosRepositorio metodo ObtenerLibros

---
+++ Datos/Repository/LibrosRepisitorio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos.Infrastructure;
+
+namespace Datos.Repository
+{
+    public class LibrosRepisitorio
+    {
+        public List<Libro> ObtenerLibros()
+        {
+            List<Libro> listadoRetorno = new List<Libro>();
+            try
+            {
+                using( var contexto = new BibliotecaEntities())
+                {
+                   listadoRetorno= contexto.Libroes.ToList();
+                }
+                return listadoRetorno;
+            }
+            catch
+            {
+
+                return listadoRetorno;
+            }
+        }
+        public List<Libro> 
+    }
+}
