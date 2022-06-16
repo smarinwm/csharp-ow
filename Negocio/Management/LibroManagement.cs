ecf5eb9ea072ddd04c6978ace96a46df4d48a896
SilverioUserSvrDev <SilverioUserSvrDev@WINDEV2204EVAL>
2022-06-16 13:06:29 +0200

    Add Method in LibroManagement -> ObtenerLibros

---
+++ Negocio/Management/LibroManagement.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos.Repository;
+using Datos.Infrastructure;
+using Negocio.EntitiesDTO;
+
+namespace Negocio.Management
+{
+    public class LibroManagement
+    {
+
+        public List<LibrosDTO> ObtenerLibros()
+        {
+
+            List<Libro> LibrosDatos = new Datos.Repository.LibrosRepositorio().ObtenerLibros();
+            List<LibrosDTO> ListadoRetorno = new List<LibrosDTO>();
+
+                foreach (var item in LibrosDatos)
+            {
+                LibrosDTO librosDTO = new LibrosDTO();
+
+                librosDTO.idLibro = item.idLibro;
+                librosDTO.Nombre = item.Nombre;
+                librosDTO.Autor = item.Autor;
+                librosDTO.FechaPublicacion = item.FechaPublicacion;
+                librosDTO.idCategoria= item.idCategoria;
+
+                ListadoRetorno.Add(librosDTO);
+
+            }
+            return ListadoRetorno;
+        }
+
+
+    }
+}
