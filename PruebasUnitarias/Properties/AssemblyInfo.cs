c0a498952e55d8f36e8517c8e6467be34f7b7c66
SilverioUserSvrDev <SilverioUserSvrDev@WINDEV2204EVAL>
2022-06-21 23:44:58 +0200

    Add Pruebas Unitarias.

---
+++ PruebasUnitarias/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PruebasUnitarias")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PruebasUnitarias")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cefea590-aed2-4817-8981-92b48961d26c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PruebasUnitarias/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Negocio;
+
+namespace PruebasUnitarias
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AltaLibro()
+        {
+
+            try
+            {
+                Negocio.EntitiesDTO.LibrosDTO libroEjemplo = new Negocio.EntitiesDTO.LibrosDTO();
+                libroEjemplo.Nombre = "Prueba";
+                libroEjemplo.Autor = "Autor";
+                libroEjemplo.FechaPublicacion = DateTime.Now;
+                libroEjemplo.idCategoria = 1;
+
+                new Negocio.Management.LibroManagement().AltaLibro(libroEjemplo);
+            }
+            catch (Exception)
+            {
+                Assert.IsTrue(false, "Alta de libro ERROR");
+                throw;
+            }
+        }
+    }
+}
